f754711482774231fe2a7f3b226879d5e22fbc63
Stéphane Dai <daistephane5@gmail.com>
2025-02-18 21:01:10 +0100

    save

--- script.py
+++ script.py
@@ -1,12 +1,18 @@
 #!/myenv/bin/python
 
 
-from socket import socket, AF_INET, SOCK_STREAM 
-import sys  
-import re  
-import datetime  
-import base64 
-import webcolors  
+from socket import socket, AF_INET, SOCK_STREAM
+import sys
+import re
+import datetime
+import base64
+import webcolors
+from pycipher import Caesar
+import nltk
+from nltk.corpus import words, stopwords
+nltk.download("words")
+
+english_words = set(words.words())
 
 
 class Answer:
@@ -19,21 +25,25 @@ class Answer:
         """Send a message to the server and return the response"""
         self.client.sendall(message.encode("utf-8"))  # Send the message
         check = self.client.recv(4096).decode("utf-8")  # Receive the response
-        return check.split()[-1] if split_last else check  # Return the full response or the last word
+        return (
+            check.split()[-1] if split_last else check
+        )  # Return the full response or the last word
 
-    def save_answers(self, answers):
+    def save_answers(self, answers, name):
         """Save answers to a file"""
-        with open("answers.txt", "a") as file:
+        with open(f"{name}.txt", "a") as file:
             file.write(answers + "\n")
 
-    def reset_answers_file(self):
+    def reset_answers_file(self, name):
         """Reset the answers file"""
-        with open("answers.txt", "w") as file:
+        with open(f"{name}.txt", "w") as file:
             file.truncate(0)
 
     def solve_math(self, response):
         """Detect a math operation in the response, solve it, and return the answer"""
-        match = re.search(r"(\d+)\s*([\+\-\*])\s*(\d+)", response)  # Search for a math operation
+        match = re.search(
+            r"(\d+)\s*([\+\-\*])\s*(\d+)", response
+        )  # Search for a math operation
         if match:
             num1, operator, num2 = (
                 int(match.group(1)),
@@ -42,9 +52,7 @@ class Answer:
             )
             result = eval(f"{num1} {operator} {num2}")  # Solve the operation
 
-            self.save_answers(str(result))  # Save the result in the file
-            with open("question_3.txt", "w") as file:
-                file.write(str(result))
+            self.save_answers(str(result), "answers")  # Save the result in the file
 
             return self.send_receive(
                 str(result), split_last=True
@@ -61,14 +69,20 @@ class Answer:
 
         for decode in encodings:
             try:
-                decoded_message = decode(response.encode()).decode()  # Decode the message
-                self.save_answers(str(decoded_message))  # Save the decoded message
+                decoded_message = decode(
+                    response.encode()
+                ).decode()  # Decode the message
+                self.save_answers(
+                    str(decoded_message), "answers"
+                )  # Save the decoded message
                 return self.send_receive(
                     str(decoded_message), split_last=True
                 )  # Return the decoded message
             except Exception:
                 continue
-        return self.reset_answers_file()  # If decoding fails, reset the answers file
+        return self.reset_answers_file(
+            "answers"
+        )  # If decoding fails, reset the answers file
 
     def morser(self, morse_message):
         """Decode a morse message"""
@@ -115,7 +129,7 @@ class Answer:
         decoded_message = "".join(
             morse_dict.get(code, "") for code in morse_message.split()
         )
-        self.save_answers(str(decoded_message))  # Save the decoded message
+        self.save_answers(str(decoded_message), "answers")  # Save the decoded message
         return decoded_message
 
     def braille(self, braille_message):
@@ -153,7 +167,9 @@ class Answer:
         new_braille_message = "".join(
             braille_dict.get(letter, "") for letter in braille_message.split()
         )
-        self.save_answers(str(new_braille_message))  # Save the decoded braille message
+        self.save_answers(
+            str(new_braille_message), "answers"
+        )  # Save the decoded braille message
         return new_braille_message
 
     def hexa(self, response):
@@ -175,7 +191,7 @@ class Answer:
             rgb_parsing = rgb.strip("()").split(",")
             new_rgb = [int(value.strip()) for value in rgb_parsing]
             color_name = webcolors.rgb_to_name(new_rgb)  # Convert RGB to color name
-            self.save_answers(str(color_name))  # Save the color name
+            self.save_answers(str(color_name), "answers")  # Save the color name
             return self.send_receive(color_name, split_last=True)
         return self.reset_answers_file()  # If the color is invalid, reset the file
 
@@ -186,28 +202,48 @@ class Answer:
             lines = file.readlines()
             if line_number <= len(lines):
                 line = lines[line_number - 1].strip()
-                self.save_answers(str(line))  # Save the returned line
+                self.save_answers(str(line), "answers")  # Save the returned line
                 return self.send_receive(line)
 
     def find(self, response):
         """Search for a specific word in a list"""
-        match = re.findall(r"[1-7](?=ème)", response)  # Search for numbers 1-7 followed by "ème"
+        match = re.findall(
+            r"[1-7](?=ème)", response
+        )  # Search for numbers 1-7 followed by "ème"
         if match:
+            self.save_answers(match[0], "caesar")
             new_response = response.split()
             number = int(match[0])
             start_index = new_response.index("liste:") + 1
             real_words = new_response[start_index:]
             letter = real_words[number - 1]
             last_letter = letter[-1]
-            self.save_answers(last_letter)  # Save the last letter
+            self.save_answers(last_letter, "answers")  # Save the last letter
             return self.send_receive(last_letter)
 
     def all_answers(self):
         """Retrieve all answers and format them into a single string"""
         with open("answers.txt", "r") as file:
             responses = file.readlines()
-        formatted_response = "_".join(response.strip() for response in responses)  # Format responses
-        return self.send_receive(formatted_response, split_last=True)  # Send the formatted response
+        formatted_response = "_".join(
+            response.strip() for response in responses
+        )  # Format responses
+
+        return self.send_receive(
+            formatted_response, split_last=True
+        )  # Send the formatted response
+
+    def cesar_code(self, response):
+        print(response)
+        for shift in range(1, 26):
+            decrypted_text = Caesar(key=shift).decipher(response).lower()
+
+            words_in_text = decrypted_text.split()
+            if any(word in english_words for word in words_in_text):
+                new_response = decrypted_text
+
+                return print(self.send_receive(new_response))
+        return None
 
 
 # Counter management functions
@@ -215,7 +251,9 @@ def reader():
     """Read the counter from a file"""
     with open("counter.txt", "r") as file:
         content = file.read().strip()  # Read the content
-        return int(content) if content.isdigit() else 10000  # Return 10000 if counter is invalid
+        return (
+            int(content) if content.isdigit() else 10000
+        )  # Return 10000 if counter is invalid
 
 
 def save(counter):
@@ -241,7 +279,12 @@ def main():
     client.bind(("0.0.0.0", local_port))  # Bind to local port
     client.connect(("148.113.42.34", target_port))  # Connect to server
 
-    responses = ["Stephane/dai/3SI5", datetime.datetime.now().strftime("%d/%m"), "", ""]  # Initial responses
+    responses = [
+        "Stephane/dai/3SI5",
+        datetime.datetime.now().strftime("%d/%m"),
+        "",
+        "",
+    ]  # Initial responses
 
     answer = Answer(client)
 
@@ -249,8 +292,8 @@ def main():
         server_response = answer.send_receive(message)
 
         if message == "Stephane/dai/3SI5":
-            answer.save_answers("Stephane/dai/3SI5")
-            answer.save_answers(datetime.datetime.now().strftime("%d/%m"))
+            answer.save_answers("Stephane/dai/3SI5", "answers")
+            answer.save_answers(datetime.datetime.now().strftime("%d/%m"), "answers")
 
         # Process server responses
         if re.search(r"(\d+)\s*[\+\-\*]\s*(\d+)", server_response):
@@ -267,11 +310,18 @@ def main():
                                 remember = answer.rewind(point)  # Rewind to get a line
                                 if re.search(r"[1-7](?=ème)", remember):
                                     answer.find(remember)  # Search for the letter
-                                    answer.all_answers()  # Return all answers
+                                    sumury = answer.all_answers()  # Return all answers
+                                    if sumury:
+                                        answer.cesar_code(sumury)
+                                        
                                 else:
                                     print("wrong answers")
-                                    answer.reset_answers_file()  # Reset if needed
+                                    answer.reset_answers_file("answers")  # Reset if needed
+                                    return None
                 else:
                     print("wrong answers")
-                    answer.reset_answers_file()  # Reset if decoding fails
+                    answer.reset_answers_file("answers")  # Reset if decoding fails
+                    return None
+
+
 main()  # Run the program
